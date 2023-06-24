3d2f1f2919b73879d165c2dc84dbf26f02971c89
Babita <mahijakhar23@gmail.com>
2023-06-24 19:12:32 +0530

    new file:   .gitignore 	modified:   package-lock.json 	modified:   src/apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "84fa3256ce7ada5731a31e4fb4a6475e",
   base: "https://api.openweathermap.org/data/2.5/",
 };
